805fa06609123a6de972dae546ce3c7044cd75f4
KI YOUNG KOWN <kky@L-DENNIS-01-AMACPRO.local>
2020-06-23 23:02:04 +0900

    20200617 진행내용

--- Client/src/Component/Main.js
+++ Client/src/Component/Main.js
@@ -1,41 +1,40 @@
-import React, { Component, PropTypes, useState, useEffect } from "react";
-import gql from "graphql-tag";
-import { useLocalStorage } from "../Basic/Utils";
-import Sidebar from "./Sidebar";
-import { useMutation } from "@apollo/react-hooks";
+import React, { Component, PropTypes, useState, useEffect } from "react"
+import gql from "graphql-tag"
+import { useLocalStorage } from "../Basic/Utils"
+import Sidebar from "./Sidebar"
+import { useMutation } from "@apollo/react-hooks"
 import { useQuery } from "@apollo/react-hooks"
-import Login from "./login";
+import Login from "./login"
 
 const SIGNIN_TOKEN = gql`
   query signInToken($Token: String!) {
     signInToken(Token: $Token) {
-      Token
+      AccessToken
     }
   }
-`;
+`
 
 function Main({ history }) {
-  const token = window.localStorage.getItem('auth')
+  const token = window.localStorage.getItem("auth")
   debugger
-  const {loading, error, data} = useQuery(SIGNIN_TOKEN, {
+  const { loading, error, data } = useQuery(SIGNIN_TOKEN, {
     variables: { Token: token },
-  });
-  if(loading) return <p>로딩 중...</p>
-  else if(error){ 
+  })
+  if (loading) return <p>로딩 중...</p>
+  else if (error) {
     debugger
     console.log("asdasd2")
-    return <Login></Login>;
+    return <Login></Login>
     /*return (<p>{error.graphQLErrors.map(({ message }, i) => (
       <span key={i}>{message}</span>))}</p>)*/
-  }
-  else if (data.signInToken.Token) {
+  } else if (data.signInToken.AccessToken) {
     debugger
-    window.localStorage.setItem('auth', data.signInToken.Token);
-    return <Sidebar></Sidebar>;
-  }else{
+    window.localStorage.setItem("auth", data.signInToken.AccessToken)
+    return <Sidebar></Sidebar>
+  } else {
     console.log("asdasd")
-    return <Login></Login>;
+    return <Login></Login>
   }
 }
 
-export default Main;
+export default Main
