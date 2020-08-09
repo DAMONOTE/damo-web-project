b424ee2ea37be33582ebb73a551f620059f347ee
paran_son <paran_son@outlook.com>
2020-08-09 12:16:00 +0900

    feat: 가입시 GROUP 생성

--- Server/api/User/SignUpUser/signUpUser.js
+++ Server/api/User/SignUpUser/signUpUser.js
@@ -4,6 +4,7 @@
 
 import user from '../../../models/User'
 import bcrypt from 'bcrypt'
+import group from '../../../models/Group'
 
 export default {
   Mutation: {
@@ -11,16 +12,22 @@ export default {
         let date = new Date()
         let createdDate = date
         let modifiedDate = date
-        let gid = []
+        let gid = Array()
         Password=bcrypt.hashSync(Password,1)
-        // ID 중복 확인
         const checkId = await user.findOne({AccountName})
         if(!checkId){
-          return await user.create({AccountName, Password, Name, Email, gid,createdDate, modifiedDate});
+          let res_user = await user.create({ AccountName, Password, Name, Email, gid, createdDate, modifiedDate })
+          let res_group = await group.create({ GroupMasterID: res_user._id, Members: [res_user._id], GroupName: AccountName })
+          gid.push(res_user._id)
+          let res_user_update = await user.updateOne({_id:res_user._id}, {$push: {Gid: gid}})
+
+          console.log(res_user)
+          return await user.findById(res_user._id)
+          //return res_user
+
         } else{
-          throw new Error("This ID already exists.")
+          throw new Error("[DEBUG] This ID already exists.")
         }
-        
     }
   }
 };
