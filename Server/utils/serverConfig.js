54118e0dfc65396f18b288d64ade556b78c7f533
paran_son <paran_son@outlook.com>
2020-08-12 21:24:06 +0900

    20200812-ImageUpload 추가

--- Server/utils/serverConfig.js
+++ Server/utils/serverConfig.js
@@ -3,8 +3,7 @@ export var serverSecretKey = "test"
 
 // DB settings
 const masterDbName = "cnt2020_db_dev"
-const masterDbUser = "accountUser"
-const masterDbPwd = "count2020"
-export const masterDbUri =`mongodb://${masterDbUser}:${masterDbPwd}@localhost:27017/${masterDbName}?retryWrites=true&w=majority`
-export const redisSecretKey = "changeme"
+//const masterDbUser = "admin"
+//const masterDbPwd = "admin"
+export const masterDbUri =`mongodb://localhost:27017/${masterDbName}?retryWrites=true&w=majority`
 
