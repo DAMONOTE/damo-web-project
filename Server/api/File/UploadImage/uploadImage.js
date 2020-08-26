df883bc75bffba1a72f03d74ee83d1f886fadb66
paran_son <paran_son@outlook.com>
2020-08-26 21:30:11 +0900

    fix: upload 기능 추가

--- Server/api/File/UploadImage/uploadImage.js
+++ Server/api/File/UploadImage/uploadImage.js
@@ -1,12 +1,12 @@
 export default{
     Mutation:{
         uploadImage: async (obj, {file}) =>{
-            const {filename, mimetype,encoding}= await file;
-            /*
-            const stream = createReadStream();
-            */
-           const returnFile = {filename, mimetype,encoding};
-           return returnFile;
+            var {createReadStream, filename, mimetype,encoding} = await file;
+            const stream = await createReadStream();
+            await stream.createWriteStream(`/Users/greendot/works/DAMONote/Server${filename}`)
+            console.log("uploaded!")
+            const returnFile = {filename, mimetype,encoding};
+            return returnFile;
         }
     }
 };
