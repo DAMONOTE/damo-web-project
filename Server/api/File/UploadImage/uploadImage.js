f63813b7a2e472415ad003380d9fd808602bb770
paran_son <paran_son@outlook.com>
2020-09-08 18:29:45 +0900

    test

--- Server/api/File/UploadImage/uploadImage.js
+++ Server/api/File/UploadImage/uploadImage.js
@@ -1,10 +1,11 @@
 export default{
     Mutation:{
         uploadImage: async (obj, {file}) =>{
-            var {createReadStream, filename, mimetype,encoding} = await file;
-            const stream = await createReadStream();
-            await stream.createWriteStream(`/Users/greendot/works/DAMONote/Server${filename}`)
+            var {filename, mimetype,encoding} = await file;
+            //const stream = await createReadStream();
+            //await stream.createWriteStream(`/Users/greendot/works/DAMONote/Server${filename}`)
             console.log("uploaded!")
+            filename = "Dennis"
             const returnFile = {filename, mimetype,encoding};
             return returnFile;
         }
